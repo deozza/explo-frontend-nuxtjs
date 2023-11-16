35e9e5c28934e1f462526514c826308311ea13b1
Edenn Touitou <edenn.touitou@letsjam.fr>
2023-11-16 12:13:51 +0000

    init

---
+++ components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+    <button>
+        {{ contentOfButton }}
+    </button>
+</template>
+
+
+<script lang="ts">
+export default {
+  name: 'ButtonComponent',
+  props: [
+    'contentOfButton'
+  ]
+}
+</script>
+
+
+<style scoped>
+    button {
+        background-color: red;
+        padding: 10px 5px;
+        margin: 5px;
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <p>bonjour</p>
+        <p>Compteur : {{ counter }}</p>
+        <p>Double : {{ double }}</p>
+        <button @click="increment">Cliquer pour incrémenter</button>
+        <button @click="decrement">Cliquer pour décrémenter</button>
+        <Button contentOfButton="Texte passé en props" ></Button>
+    </div>
+</template>
+
+<script lang="ts">
+import { computed, ref } from 'vue'
+
+export default {
+  name: 'IndexPage',
+  setup() {
+    const counter = ref(0);
+    const double = computed(() => counter.value * 2);
+
+    return {
+        counter,
+        double
+    }
+  },
+  methods:{
+    increment(){
+      this.counter++
+    },
+    decrement(){
+      this.counter--
+    }
+  }
+}
+</script>
+
+<style>
+    button {
+        background-color: greenyellow;
+        padding: 10px 5px;
+        margin: 5px;
+    }
+</style>
